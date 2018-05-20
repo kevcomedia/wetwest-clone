61dec60f5a3e5ec01d56fe4976e98c2ddc3ef753
Kev Comedia <kevcomedia@gmail.com>
2018-05-20 21:06:33 +0800

    Build project

---
+++ docs/scripts/navbar.js
@@ -0,0 +1,2 @@
+"use strict";var collapseToggler=document.querySelector(".navbar__collapse-toggler"),list=document.querySelector(".navbar__list");collapseToggler.addEventListener("click",function(){list.classList.toggle("navbar__list_active")});
+//# sourceMappingURL=navbar.js.map
